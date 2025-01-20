8761a2d5563683a3c6e15c72b0cec39768e01c95
Ruth Marks <ruthmarks151@gmail.com>
2025-01-20 17:14:32 -0500

    Fix linting

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess({ script: true }),
+	preprocess: vitePreprocess({ script: true }),
 
-    kit: {
-        adapter: adapter(),
-				paths: {
-            base: process.env.NODE_ENV === 'production' ? '/congruence' : '',
-        }
-    }
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/congruence' : ''
+		}
+	}
 };
 
 export default config;
